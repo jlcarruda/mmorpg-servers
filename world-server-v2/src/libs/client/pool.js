6c67ae00f30bb337530c3b1b2830f552c2cab1be
João Lucas de Carvalho Arruda <jlcarruda3@gmail.com>
2021-04-12 18:07:59 -0300

    refactor: added client factory and documentation

--- world-server-v2/src/libs/client/pool.js
+++ world-server-v2/src/libs/client/pool.js
@@ -1,9 +1,19 @@
 const { getClient } = require('../../repositories/redis')
+const Factory = require('./factory')
 
 let _storageClient
 let _instance
+let _maxClients = 50
+/**
+ * Singleton class that will handle the connected clients pool
+ */
 class ClientPool {
 
+  /**
+   * Retrieve the already created instance of ClientPool.
+   * If not created, it calls `.create()`
+   * @returns {ClientPool}
+   */
   static getInstance() {
     if (!_instance) {
       const errMessage = "[CLIENT POOL] - instance not defined. Please create an instance before using"
@@ -14,6 +24,14 @@ class ClientPool {
     }
   }
 
+  /**
+   * Create the ClientPool instance.
+   * If its already created, returns the singleton instance.
+   * If not, it will create a new one.
+   *
+   * If `storageClient` is not passed, it will use the default client
+   * @param {} storageClient - an object that handles the storage of client objects
+   */
   static create(storageClient) {
     if (!_instance) {
       if (!storageClient) {
@@ -57,6 +75,11 @@ class ClientPool {
     }
   }
 
+  /**
+   * Retrieves the client object from the pool
+   * @param {string} id - Client Identifier
+   * @returns {Promise<boolean | JSON>} The Client object. If fails, returns a `false`
+   */
   async findById(id) {
     try {
       // Check if pool already has the client
@@ -68,7 +91,7 @@ class ClientPool {
       client = await _storageClient.get(id)
       if (client) {
         this.poolAdd(client)
-        return JSON.parse(client);
+        return Factory.deserialize(client);
       }
       return false
     } catch (err) {
@@ -78,6 +101,11 @@ class ClientPool {
     return false
   }
 
+  /**
+   * Removes the client from the pool
+   * @param {string} id - Client Identifier
+   * @returns {Promise<boolean>} indicates if the operation succeeded or not
+   */
   async remove(id) {
     try {
       this.poolRemove(id)
@@ -89,8 +117,17 @@ class ClientPool {
     return false
   }
 
+  /**
+   * Update the client object in the pool.
+   * It will rewrite all the object in which its key (`id`) matched.
+   * @param {string | JSON} client - Client object
+   * @returns {Promise<boolean>} indicates if the operation succeeded or not
+   */
   async update(client) {
     try {
+      if (typeof client === 'string') {
+        client = Factory.deserialize(client)
+      }
       const c = await _storageClient.get(client.id)
       if (c) {
         this.poolUpdate(client)
@@ -99,7 +136,7 @@ class ClientPool {
         const lastUpdate = new Date(c.lastUpdatedAt)
         const diffTime = Math.abs(today - lastUpdate)
         if (Math.ceil(diffTime / (1000 * 60) >= 5)) { // 5 minutes
-          await redis.set(client.id, JSON.stringify(client))
+          await _storageClient.set(client.id, Factory.serialize(client))
         }
         return true
       }
@@ -110,10 +147,15 @@ class ClientPool {
     return false
   }
 
+  /**
+   * Add the object into the pool of clients, serializing it first
+   * @param {JSON} client - Client object to be saved on pool
+   * @returns {boolean} indicates if the operation succeeded or not
+   */
   async add(client) {
     try {
       this.poolAdd(client)
-      await _storageClient.set(client.id, JSON.stringify(client))
+      await _storageClient.set(client.id, Factory.serialize(client))
       return true
     } catch (error) {
       console.error('[GAMEWORLD] Error while adding client to the pool', error)
