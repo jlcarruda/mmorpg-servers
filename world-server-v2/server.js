1ec68f8288e80a637e586b66f18af1d3c2194c5d
João Lucas de Carvalho Arruda <jlcarruda3@gmail.com>
2021-04-12 11:39:39 -0300

    refactor: initial file structure

--- world-server-v2/server.js
+++ world-server-v2/server.js
@@ -1,38 +1,46 @@
 const config = require('./config')
-const { initialize: serverInitializer } = require('./src/initializer')
-const { initialize: databaseInitializer } = require('./src/database')
-const createRedisClient = require('./src/redis')
-const WorldQueues = require('./src/queue')
-const { posUpdateHandle, charPersistHandle } = require('./src/queue-handles')
 
 const { server, database } = config
-console.log("Configs Initialized", JSON.stringify(config))
-console.log('[GAMEWORLD] Initializing server ...')
+
 module.exports = (async () => {
-
-  try {
-    // const redisClient =  new Redis(redisPort, redisHost, { password }) //redis.createClient(clientConfig)
-    const redisClient = await createRedisClient()
-
-    const sharedRedisConfig = {
-      redis: redisClient
-    }
-
-    await databaseInitializer(database)
-    // Create queues
-    const config = {
-      ...sharedRedisConfig,
-      removeOnSuccess: true,
-      removeOnFailure: true,
-    }
-    await WorldQueues.createQueue("POS_UPDATE_Q", posUpdateHandle, config)
-    // await WorldQueues.createQueue("CHAR_UPDATE_Q", charUpdateHandle, config)
-    await WorldQueues.createQueue("CHAR_PERSIST_Q", charPersistHandle, config)
-
-    await serverInitializer(server, redisClient)
-
-  } catch (err) {
-    console.error("Error while trying to initialize server", err)
-    throw err
-  }
+  
 })()
+
+// const config = require('./config')
+// const { initialize: serverInitializer } = require('./src/initializer')
+// const { initialize: databaseInitializer } = require('./src/database')
+// const createRedisClient = require('./src/redis')
+// const WorldQueues = require('./src/queue')
+// const { posUpdateHandle, charPersistHandle } = require('./src/queue-handles')
+
+// const { server, database } = config
+// console.log("Configs Initialized", JSON.stringify(config))
+// console.log('[GAMEWORLD] Initializing server ...')
+// module.exports = (async () => {
+
+//   try {
+//     // const redisClient =  new Redis(redisPort, redisHost, { password }) //redis.createClient(clientConfig)
+//     const redisClient = await createRedisClient()
+
+//     const sharedRedisConfig = {
+//       redis: redisClient
+//     }
+
+//     await databaseInitializer(database)
+//     // Create queues
+//     const config = {
+//       ...sharedRedisConfig,
+//       removeOnSuccess: true,
+//       removeOnFailure: true,
+//     }
+//     await WorldQueues.createQueue("POS_UPDATE_Q", posUpdateHandle, config)
+//     // await WorldQueues.createQueue("CHAR_UPDATE_Q", charUpdateHandle, config)
+//     await WorldQueues.createQueue("CHAR_PERSIST_Q", charPersistHandle, config)
+
+//     await serverInitializer(server, redisClient)
+
+//   } catch (err) {
+//     console.error("Error while trying to initialize server", err)
+//     throw err
+//   }
+// })()
